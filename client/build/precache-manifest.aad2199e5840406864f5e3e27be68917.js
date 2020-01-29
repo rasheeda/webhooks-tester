252fbc9b06da80925ca2f7ec61b016c92d8acc0b
Rasheeda Yehuza <ryehuza@gmail.com>
2020-01-29 19:48:11 +0100

    removing build folder ignore due to issues with npm on prod

---
+++ client/build/precache-manifest.aad2199e5840406864f5e3e27be68917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c630a77cb62734c678856daefddc68a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f48fd219563745f5d57d",
+    "url": "/static/css/main.00e1c300.chunk.css"
+  },
+  {
+    "revision": "30d662ff3c13bbbed3c8",
+    "url": "/static/js/2.bcb98a01.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.bcb98a01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f48fd219563745f5d57d",
+    "url": "/static/js/main.8979dec5.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
