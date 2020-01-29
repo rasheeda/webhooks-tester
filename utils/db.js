5c75a6363d29c48c50c119200d14e84d11b355e8
Rasheeda Yehuza <ryehuza@gmail.com>
2020-01-29 18:19:18 +0100

    adding .env files

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,22 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "mandeeya",
-  password: "pass",
-  database: "webhookz"
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
 
-  console.log("connected as id " + connection.threadId);
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
